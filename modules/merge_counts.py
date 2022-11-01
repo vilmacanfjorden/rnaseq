6fa13d3365a057e0a95445ca183d6aed51c133b7
Vilma Canfjorden <vilma.canfjorden@gu.se>
2022-11-01 12:30:26 +0100

    sorted counts file

--- modules/merge_counts.py
+++ modules/merge_counts.py
@@ -5,8 +5,8 @@ import pandas as pd
 
 # HTseq count
 def merge_counts(args):
-    # Filename of files with counts
-    filenames = glob.glob(f"{args.samples}/*.counts", recursive=True)
+    # Filename of files with counts and sort filenames
+    filenames = sorted(glob.glob(f"{args.outputdir}/results/*.counts", recursive=True))
 
     # Merge count files
     print("Merging count files...")
@@ -18,10 +18,10 @@ def merge_counts(args):
         h = os.path.basename(m.replace(".counts", "")) # Get name from headers
         header.append(h)
 
-        values = pd.read_csv(m, sep="\t", index_col=0) # Get values make ENSEMBL_ID to index to remove duplicate columns
+        values = pd.read_csv(m, sep="\t", index_col=0, header=None) # Get values make ENSEMBL_ID to index to remove duplicate columns
         frames.append(values)
 
     df = pd.concat(frames, axis=1) # Concatenate the data from different files
-    df.reset_index(inplace=True) # Remove index
+    df.reset_index(inplace=True, level=0) # Remove index
     df.columns = header # Add the headers (sample names)
-    df.to_csv(f"{args.outputdir}/results/counts.txt", sep='\t', index=False) # Write dataframe to a txt file
+    df.to_csv(f"{args.outputdir}/results/counts.txt", sep=" ", index=False) # Write dataframe to a txt file
